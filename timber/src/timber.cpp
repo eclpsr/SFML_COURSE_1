1446721656c41687a37d219ea1e4741bb44859c4
Artem Reshetnikov <balupwns@gmail.com>
2021-03-24 11:38:41 +0900

    27. Growing the Branches

--- timber/src/timber.cpp
+++ timber/src/timber.cpp
@@ -3,6 +3,15 @@
 
 using namespace sf;
 
+// Function declaration
+void updateBranches(int seed);
+const int NUM_BRANCHES = 6;
+Sprite branches[NUM_BRANCHES];
+// Where is the player/branch?
+// Left or Right
+enum class side { LEFT, RIGHT, NONE };
+side branchPositions[NUM_BRANCHES];
+
 int main() {
 
 	// Create a video mode object
@@ -116,6 +125,24 @@ int main() {
 	messageText.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
 	messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
 	scoreText.setPosition(20, 20);
+
+	// Prepare 6 branches
+	Texture textureBranch;
+	textureBranch.loadFromFile("graphics/branch.png");
+	// Set the texture for each branch sprite
+	for (int i = 0; i < NUM_BRANCHES; i++){
+		branches[i].setTexture(textureBranch);
+		branches[i].setPosition(-2000, -2000);
+		// Set the sprite's origin to dead center
+		// We can then spin it round without changing its position
+		branches[i].setOrigin(220, 20);
+	}
+
+	updateBranches(1);
+	updateBranches(2);
+	updateBranches(3);
+	updateBranches(4);
+	updateBranches(5);
 
 	while (window.isOpen()){
 		if (Keyboard::isKeyPressed(Keyboard::Escape)){
@@ -246,7 +273,26 @@ int main() {
 		ss << "Score = " << score;
 		scoreText.setString(ss.str());
 
+		// update the branch sprites
+		for (int i = 0; i < NUM_BRANCHES; i++){
+			float height = i * 150;
+			if (branchPositions[i] == side::LEFT){
+				// Move the sprite to the left side
+				branches[i].setPosition(610, height);
+				// Flip the sprite round the other way
+				branches[i].setRotation(180);
+			} else if (branchPositions[i] == side::RIGHT) {
+				// Move the sprite to the right side
+				branches[i].setPosition(1330, height);
+				// Set the sprite rotation to normal
+				branches[i].setRotation(0);
+			} else {
+				// Hide the branch
+				branches[i].setPosition(3000, height);
+			}
 		}
+
+		} //?
 
 		//Clear everything from the last frame
 		window.clear();
@@ -258,6 +304,11 @@ int main() {
 		window.draw(spriteCloud1);
 		window.draw(spriteCloud2);
 		window.draw(spriteCloud3);
+
+		// Draw the branches
+		for (int i = 0; i < NUM_BRANCHES; i++){
+			window.draw(branches[i]);
+		}
 
 		// Draw the tree
 		window.draw(spriteTree);
@@ -279,5 +330,26 @@ int main() {
 		window.display();
 	}
 	return 0;
+}
+
+void updateBranches(int seed){
+	// Move all the branches down one place
+	for (int j = NUM_BRANCHES - 1; j > 0; j--){
+		branchPositions[j] = branchPositions[j - 1];
+	}
+	// Spawn a new branch at position 0
+	// LEFT, RIGHT or NONE
+	srand((int)time(0) + seed);
+	int r = (rand() % 5);
+	switch (r){
+	case 0:
+		branchPositions[0] = side::LEFT;
+		break;
+	case 1:
+		branchPositions[0] = side::RIGHT;
+		break;
+	default:
+		branchPositions[0] = side::NONE;
+	}
 }
 
